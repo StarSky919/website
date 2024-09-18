a062651bf7d9658e72a93e37f0a8c9969451a5ce
StarSky919 <ss919p@gmail.com>
2024-09-18 12:47:22 +0800

    QQS12

--- qqs12/install.sh
+++ qqs12/install.sh
@@ -1,7 +1,7 @@
 if test -e $PREFIX/bin/ffmpeg
 then
-  curl -s https://starsky919.xyz/qqs12/QQS -o $PREFIX/bin/QQS
-  curl -s https://starsky919.xyz/qqs12/QQSC -o $PREFIX/bin/QQSC
+  curl -fsSL https://starsky919.xyz/qqs12/QQS -o $PREFIX/bin/QQS
+  curl -fsSL https://starsky919.xyz/qqs12/QQSC -o $PREFIX/bin/QQSC
   chmod 0755 $PREFIX/bin/QQS
   chmod 0755 $PREFIX/bin/QQSC
 else
